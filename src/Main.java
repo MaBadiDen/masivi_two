0e904d31d5091a5eee3b7d7cd677945df68db1a8
MaBadiDen <kaba4ok.den@gmail.com>
2022-07-29 19:44:34 +0300

    Готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] trati = generateRandomArray();
+        int summ = 0;
+        int max = 0;
+        int min = 200000;
+
+        for (int i = 0; i < 30; i++) {
+            summ += trati[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + summ + " рублей");
+
+        for (int i = 0; i < 30; i++) {
+            if (trati[i] >= max) {
+                max = trati[i];
+            }
+            if (trati[i] <= min) {
+                min = trati[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+
+        System.out.println("Средняя сумма трат за месяц составила " + ((double)summ / 30) + "рублей");
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for(int i = reverseFullName.length - 1; i >= 0; i--)
+        {
+            System.out.print(reverseFullName[i]);
+        }
+    }
+
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
